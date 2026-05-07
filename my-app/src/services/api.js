7a8e899c2b17826f2a7344bcb57485cd5f60b080
Gerald Niño Andaya Dejumo <gndejumo@gmail.com>
2026-05-07 15:12:55 +0800

    update API baseURL for prod

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const API = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://blog-application-5s3r.onrender.com"
 })
 
 API.interceptors.request.use(config => {
